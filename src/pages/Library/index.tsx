d0c5a4c8c88c78775db1f33efa952326be5ca9a7
Emily Jiang <brilliantemilyjiang@gmail.com>
2023-12-03 23:54:14 -0500

    refactor: moved data table to component && added context menu

--- src/pages/Library/index.tsx
+++ src/pages/Library/index.tsx
@@ -1,8 +1,9 @@
 import { Add } from "@mui/icons-material";
-import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListSubheader, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
+import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListSubheader, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { FC, useState } from "react";
 import { useTranslation } from "react-i18next";
+import SongsView from "./SongsView";
 
 const LibraryPage: FC = () => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
@@ -12,23 +13,13 @@ const LibraryPage: FC = () => {
     setSelectedIndex(-1);
   }
   
-  function createData (
-    id: number,
-    name: string,
-    artist: string,
-    path: string,
-  ) {
-    return { id, name, artist, path };
-  }
-
-  // TODO: fake data now
   const rows = [
-    createData(1, "Kelly is cute", "Emily", "/home/kelly/music/1"),
-    createData(2, "Kelly is not cute", "Cat", "/home/kelly/music/1"),
-    createData(3, "Is Kelly cute", "Someone", "/home/kelly/music/1"),
-    createData(4, "Kelly is cute", "Someone", "/home/kelly/music/1"),
-    createData(5, "Kelly is cute", "", "/home/kelly/music/1"),
-    createData(6, "Kelly is cute", "null", "/home/kelly/music/1"),
+    {id: 1, name: "Kelly is cute", artist: "Emily", path: "/home/kelly/music/1"},
+    {id: 2, name: "Kelly is not cute", artist: "Cat", path: "/home/kelly/music/1"},
+    {id: 3, name: "Is Kelly cute", artist: "Someone", path: "/home/kelly/music/1"},
+    {id: 4, name: "Kelly is cute", artist: "Someone", path: "/home/kelly/music/1"},
+    {id: 5, name: "Kelly is cute", artist: "", path: "/home/kelly/music/1"},
+    {id: 6, name: "Kelly is cute", artist: "null", path: "/home/kelly/music/1"},
   ];
 
   return (
@@ -59,32 +50,7 @@ const LibraryPage: FC = () => {
           <SearchIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
           <TextField id="input-with-sx" label={t("search")} variant="standard" />
         </Box>
-
-        <TableContainer>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">{t("storage-id")}</TableCell>
-                <TableCell align="left">{t("name")}</TableCell>
-                <TableCell align="left">{t("artist")}</TableCell>
-                <TableCell align="left">{t("path")}</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => ( 
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="left">{row.id}</TableCell>
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="left">{row.artist}</TableCell>
-                  <TableCell align="left">{row.path}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <SongsView SongsInfos={rows}/>
       </Box>
 
 
